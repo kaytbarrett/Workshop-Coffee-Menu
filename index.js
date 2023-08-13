4d56ecb9b918501f7c70d30463614e1e017e5641
Katie Barrett <katiebarrett@Katies-MacBook-Air.local>
2023-08-13 17:33:06 -0600

    prompt 3 complete

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const coffeeMenu = require("./coffee_data.js");
 //Prompt 2
 //Print an array of all the drinks on the menu.
 
-const drinks = coffeeMenu.map(element => element.name)
+const drinks = coffeeMenu.map(item => item.name)
 console.log(drinks)
+
+//Prompt 3
+//Print an array of drinks that cost 5 and under.
+
+
+const underFive = (item) => {
+   if (item.price <=5) {
+    return item;
+   } 
+}
+const itemsUnderFive = coffeeMenu.filter(underFive);
+console.log(itemsUnderFive);
+
 
